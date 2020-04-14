d3a9b227308fb3b3913c235aeb5684a17486e000
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-04-14 23:18:32 +0900

    good

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.keys = ['some secret hurr'];
 const CONFIG = {
     key: 'koa:sess', 
     maxAge: 86400000,
-    overwrite: true, /** (boolean) can overwrite or not (default true) */
-    httpOnly: true, /** (boolean) httpOnly or not (default true) */
-    signed: true, /** (boolean) signed or not (default true) */
-    rolling: true, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
-    renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+    overwrite: true, 
+    httpOnly: true, 
+    signed: true, 
+    rolling: true, 
+    renew: false, 
 };
 
 
