e75f0bad32914a518bdfe8091cd8df4a85b8040a
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-04-16 14:53:01 +0900

    good

--- static_asset/my_tools/ajax_request.js
+++ static_asset/my_tools/ajax_request.js
@@ -1,4 +1,6 @@
 // let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//require 只能在node.js里面用，这个js文件是要导入到html文件里面去的所以就不能require
+//因为 浏览器是支持 XMLHttpRequest 的，所以不用require也可以用 XMLHttpRequest 的
 
 let ajax={
     toggle:()=>{
@@ -15,25 +17,6 @@ let ajax={
         })
     }
 }
-
-
-//object 上面不能这样用foreach
-// params.forEach((key,value)=>{
-//     array.push(`${key}=${value}`);
-// })
-
-//为什么要使用encodeURIComponent
-//将一些特殊的字符转码(特殊字符包括=，/,%,&,+,-之类的)
-// 为什么需要Url编码，通常如果一样东西需要编码，说明这样东西并不适合传输。
-// 原因多种多样，如Size过大，包含隐私数据，
-// 对于Url来说，之所 以要进行编码，是因为Url中有些字符会引起歧义。
-//  例如Url参数字符串中使用key=value键值对这样的形式来传参，
-//  键值对之间以&符号分隔，如/s?q=abc&ie=utf- 8。
-//  如果你的value字符串中包含了=或者&，那么势必会造成接收Url的服务器解析错误，
-//  因此必须将引起歧义的&和=符号进行转义， 也就是对其进行编码。
-// 又如，Url的编码格式采用的是ASCII码，而不是Unicode，
-// 这也就是说你不能在Url中包含任何非ASCII字符，例如中文。
-// 否则如果客户端浏 览器和服务端浏览器支持的字符集不同的情况下，中文可能会造成问题。
 
 
 
