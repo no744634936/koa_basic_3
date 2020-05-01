99e12555e5fecbc66a58dca62b673d614ad8dff4
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-05-01 15:51:42 +0900

    good

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
+var Koa=require('koa'),
+    router = require('koa-router')(),
+    views = require('koa-views'),
+    bodyParser = require('koa-bodyparser'),
+    static = require('koa-static');
 
-  const Koa = require('koa')
-  const router = require('koa-router')()
-  const app = new Koa()
+var app=new Koa();
+/*应用ejs模板引擎*/
+app.use(views('views',{
+    extension:'ejs'
+}))
 
-  router.get('/', async(ctx, next) => {
-    ctx.response.body = `<h1>index page</h1>`
-  })
+//http://localhost:3000/css/basic.css  首先去static目录找 ，如果能找到返回对应的文件，找不到 next()
 
-  router.get('/home', async(ctx, next) => {
-    console.log(ctx.request.query)
-    console.log(ctx.request.querystring)
-    ctx.response.body = '<h1>HOME page</h1>'
-  })
+//配置静态web服务的中间件
+//使用了静态资源中间件之后，view里面就可以这样引入 css 或者图片了  <link rel="stylesheet" href="css/basic.css"/>
+//koa静态资源中间件可以配置多个
 
-  router.get('/404', async(ctx, next) => {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-  })
+console.log(__dirname);  //app.js 文件所在目录   c:\Users\zhang\Desktop\koa2_basic_2
+app.use(static(__dirname+'/static'));
+app.use(static(__dirname+'/public'));   
 
-  // add router middleware:
-  app.use(router.routes())
 
-  app.listen(3000, () => {
-    console.log('server is running at http://localhost:3000')
-  })
+
+//配置post bodyparser的中间件
+app.use(bodyParser());
+router.get('/',async (ctx)=>{
+    await  ctx.render('index');
+})
+
+//接收post提交的数据
+router.post('/doAdd',async (ctx)=>{
+    console.log(ctx.request.body);
+    ctx.body=ctx.request.body;  //获取表单提交的数据
+})
+
+app.use(router.routes());   /*启动路由*/
+app.use(router.allowedMethods());
+app.listen(3000);
+
+
+
+
+
+
