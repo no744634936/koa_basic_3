c3f5f63eb15039633a72d2edeb7d56f24fac5a3e
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-05-01 14:08:47 +0900

    good

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-const Koa = require('koa')
-// 注意 require('koa-router') 返回的是函数:
-const router = require('koa-router')()
-const app = new Koa()
 
- // 添加路由
- router.get('/', async (ctx, next) => {
-    ctx.body = `<h1>index page</h1>`
+var Koa=require('koa'),
+    router = require('koa-router')(),
+    render = require('koa-art-template'),
+    path=require('path');
+
+var app=new Koa();
+
+//配置 koa-art-template模板引擎
+render(app, {
+    root: path.join(__dirname, 'views'),   // 视图的位置
+    extname: '.html',  // 后缀名
+    debug: process.env.NODE_ENV !== 'production'  //是否开启调试模式
+
+});
+
+router.get('/',async (ctx)=>{
+   //ctx.body="首页"
+    let list={
+
+        name:'张三',
+        h:'<h2>这是一个h2</h2>',
+        num:20,
+        data:['11111111','2222222222','33333333333']
+    }
+
+    await ctx.render('index',{
+        list:list
+
+    });
+})
+//接收post提交的数据
+router.get('/news',async (ctx)=>{
+
+    let app={
+
+        name:'张三11',
+        h:'<h2>这是一个h211</h2>',
+        num:20,
+        data:['11111111','2222222222','33333333333']
+    };
+    await ctx.render('news',{
+        list:app
+    });
 })
 
-router.get('/home', async (ctx, next) => {
-    ctx.body = '<h1>HOME page</h1>'
-})
 
-router.get('/404', async (ctx, next) => {
-    ctx.body = '<h1>404 Not Found</h1>'
-})
+app.use(router.routes());   /*启动路由*/
+app.use(router.allowedMethods());
+app.listen(3001, () => {
+    console.log('server is running at http://localhost:3001')
+  })
 
- // 调用路由中间件
- app.use(router.routes())
 
-app.listen(3000, ()=>{
-  console.log('server is running at http://localhost:3000')
-})
+
+
+
