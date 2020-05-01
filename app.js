70debacb744ead83bffed904016eb3288efca0f6
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-05-01 14:20:55 +0900

    good

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 
 var Koa=require('koa'),
-    router = require('koa-router')(),
     render = require('koa-art-template'),
-    path=require('path');
-
-var app=new Koa();
+    path=require('path'),
+    app=new Koa(),
+    router=require("./router/admin.js")
 
 //配置 koa-art-template模板引擎
 render(app, {
@@ -14,39 +13,8 @@ render(app, {
 
 });
 
-router.get('/',async (ctx)=>{
-   //ctx.body="首页"
-    let list={
 
-        name:'张三',
-        h:'<h2>这是一个h2</h2>',
-        num:20,
-        data:['11111111','2222222222','33333333333']
-    }
-
-    await ctx.render('index',{
-        list:list
-
-    });
-})
-//接收post提交的数据
-router.get('/news',async (ctx)=>{
-
-    let app={
-
-        name:'张三11',
-        h:'<h2>这是一个h211</h2>',
-        num:20,
-        data:['11111111','2222222222','33333333333']
-    };
-    await ctx.render('news',{
-        list:app
-    });
-})
-
-
-app.use(router.routes());   /*启动路由*/
-app.use(router.allowedMethods());
+app.use(router);
 app.listen(3001, () => {
     console.log('server is running at http://localhost:3001')
   })
